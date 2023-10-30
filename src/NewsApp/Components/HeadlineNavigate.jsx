55e4f2a251903c83dfcbe77c5b5d207a2d0610f0
JEETU <jitupanchal012@gmail.com>
2023-10-30 17:39:09 +0530

    #4 commit

--- src/NewsApp/Components/HeadlineNavigate.jsx
+++ src/NewsApp/Components/HeadlineNavigate.jsx
@@ -15,7 +15,6 @@ function HeadlineNavigate() {
             <div className='top-headline-nav mx-lg-auto'>
                 <ul className="navbar-nav me-auto mt-lg-0 mt-2 mb-lg-0">
                     <li onClick={(e) => handleClick(e)} className="nav-item"><a className={`nav-link py-1 rounded text-light ${cat === 'general' ? 'bg-light fw-bold text-cobalt ps-2' : ''}`} >General</a></li>
-                    <li onClick={(e) => handleClick(e)} className="nav-item"><a className={`nav-link py-1 rounded text-light ${cat === 'business' ? 'bg-light fw-bold text-cobalt ps-2' : ''}`} >Business</a></li>
                     <li onClick={(e) => handleClick(e)} className="nav-item"><a className={`nav-link py-1 rounded text-light ${cat === 'entertainment' ? 'bg-light fw-bold text-cobalt ps-2' : ''}`} >Entertainment</a></li>
                     <li onClick={(e) => handleClick(e)} className="nav-item"><a className={`nav-link py-1 rounded text-light ${cat === 'health' ? 'bg-light fw-bold text-cobalt ps-2' : ''}`} >Health</a></li>
                     <li onClick={(e) => handleClick(e)} className="nav-item"><a className={`nav-link py-1 rounded text-light ${cat === 'science' ? 'bg-light fw-bold text-cobalt ps-2' : ''}`} >Science</a></li>
